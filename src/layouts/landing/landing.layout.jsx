fde95cab9796537ba9c5b2118bbc0cbbbb8ffbb3
Emre Can Erol <erolemrecan@outlook.com>
2022-08-14 17:56:16 +0300

    landinge buton eklendi

--- src/layouts/landing/landing.layout.jsx
+++ src/layouts/landing/landing.layout.jsx
@@ -1,24 +1,38 @@
 import "./landing.style.scss";
+import { useContext } from "react";
+import { ButtonContext } from "../../contexts/button.context";
 import Hero from "../../images/illustration-hero.svg";
+import MediumButton from "../../components/medium-button/medium-button.component";
 
 export default function Landing() {
-    
-    return (
-        <div className="landingdiv">
-            <div className="bluesurface"></div>
-            <div className="landingleftside">
-                <h1 className="h1">A Simple Bookmark Manager</h1>
-                <p className="parag">A clean and simple interface to organize your
-                favourite websites. Open a new browser tab and
-                see your sites load instantly. Try it for free.</p>
-                <div className="landingbuttonsdiv">
-                    <button>Button1</button>
-                    <button>Button2</button>
-                </div>
-            </div>
-            <div className="landingrightside">
-                <img src={Hero} alt="illustration_tablet" className="hero"/>
-            </div>
+  const { buttonType } = useContext(ButtonContext);
+
+  const { mediumBlueButton, mediumGreyButton } = buttonType;
+
+  return (
+    <div className="landingdiv">
+      <div className="bluesurface"></div>
+      <div className="landingleftside">
+        <h1 className="h1">A Simple Bookmark Manager</h1>
+        <p className="parag">
+          A clean and simple interface to organize your favourite websites. Open
+          a new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        <div className="landingbuttonsdiv">
+          <MediumButton
+            text={"Get it on Chrome"}
+            colorName={mediumBlueButton}
+          />
+          <MediumButton
+            text={"Get it on Firefox"}
+            colorName={mediumGreyButton}
+          />
         </div>
-    )
+      </div>
+
+      <div className="landingrightside">
+        <img src={Hero} alt="illustration_tablet" className="hero" />
+      </div>
+    </div>
+  );
 }
