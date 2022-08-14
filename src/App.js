b7212dca1b1e48d7130cfa91bf53b2ee541f10bf
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2022-08-14 15:39:57 +0300

    bulut ilk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./layouts/header/header.component";
-
+import Landing from "./layouts/landing/landing.layout";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Landing />
     </div>
   );
 }
---
+++ src/layouts/landing/landing.layout.jsx
@@ -0,0 +1,9 @@
+import "./landing.style.scss";
+
+export default function landing() {
+  return (
+    <div className="landingdiv">
+        
+    </div>
+  )
+}
