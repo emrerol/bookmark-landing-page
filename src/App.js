4690d14628d6ff9897fb3d5026b625c9066784d5
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2022-08-16 23:32:35 +0300

    features part1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./layouts/header/header.layout";
 import Landing from "./layouts/landing/landing.layout";
 import "./App.css";
+import Features from "./layouts/features/features.layout";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Landing />
+      <Features />
     </div>
   );
 }
---
+++ src/layouts/features/features.layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./features.style.scss";
+export default function Features() {
+  return (
+    <div className='featuresdiv'>
+        <h1>Features</h1>
+        <p> Our aim is to make it quick and easy you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
+    </div>
+  )
+}
